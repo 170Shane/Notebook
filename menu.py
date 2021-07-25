a88b3ff64bb90eeb37c3d800967ed06f0627b9c4
shaneclark <170shane@gmail.com>
2021-07-25 12:11:08 +0100

    Further changes to Notebook application

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import sys
-from notebook import Notebook, Note
+from notebook import Notebook
 
 
 class Menu:
@@ -11,7 +11,7 @@ class Menu:
             "1": self.show_notes,
             # "2": self.search_notes,
             "3": self.add_note,
-            # "4": self.modify_note,
+            "4": self.modify_note,
             "5": self.quit
         }
 
@@ -41,8 +41,10 @@ class Menu:
     def show_notes(self, notes=None):
         if not notes:  # if we are not being passed a list of notes that have been filtered for a match
             notes = self.notebook.notes  # display all notes in the Notebook
-        for note in notes:  # loop through the notes collection
+        for note in notes:  # loop through the notes collection which will be either all notes or notes that
+            # have matched the search pattern
             print(f"{note.id}: {note.tags}\n{note.memo}")  # print out the notes and some additional details
+
     #
     # def search_notes(self):
     #     filter = input("Search for: ")
@@ -54,15 +56,22 @@ class Menu:
         tags = input("Hit me with some tags: ")
         self.notebook.new_note(memo, tags)
         print("Your note has been added")
-    #
-    # def modify_note(self):
-    #     id = input("Enter a note ID: ")
-    #     memo = input("Enter a memo: ")
-    #     tags = input("Enter tags: ")
-    #     if memo:
-    #         self.notebook.modify_memo(id, memo)
-    #     if tags:
-    #         self.notebook.modify_tags(id, tags)
+
+    def modify_note(self):
+        try:
+            id = int(input("Enter a note ID: "))
+            # do we have a valid ID?
+            if self.notebook._find_note(id) is not None:
+                memo = input("Enter a memo: ")
+                tags = input("Enter tags: ")
+                if memo:
+                    self.notebook.modify_memo(id, memo)
+                if tags:
+                    self.notebook.modify_tags(id, tags)
+        except ValueError:
+            print('Incorrect Note Value entered. Please try again')
+        except Exception as e:
+            print(e)
 
     def quit(self):
         print("Thank you for using your notebook today.")
